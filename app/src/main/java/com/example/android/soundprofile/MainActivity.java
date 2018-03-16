178900a0b7dce3a1923459f55d0bb451d2f9e7e7
sincemcmv <fikretsomay@gmail.com>
2018-03-16 23:53:41 +0300

    ikinci kayıt

--- app/src/main/java/com/example/android/soundprofile/MainActivity.java
+++ app/src/main/java/com/example/android/soundprofile/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.soundprofile;
 import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_is_clicked(View v){
+
     }
 }
